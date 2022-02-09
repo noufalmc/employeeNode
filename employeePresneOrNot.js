5cb868d580ce610c318d9e90801980c10117e992
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2022-02-09 16:03:39 +0530

    Added:Function For get Hour

--- employeePresneOrNot.js
+++ employeePresneOrNot.js
@@ -5,7 +5,14 @@ const IS_FULLTIME_HOUR=8;
 const IS_PARTTIME_HOUR=4;
 const IS_WAGE_PERHOUR=20;
 let empCheck=Math.floor(Math.random()*10)%3;
-let hour=0;
+let wage=IS_WAGE_PERHOUR*getHour();
+console.log("Wage is"+wage);
+
+
+
+function getHour()
+{
+    let hour=0;
 if(empCheck==IS_PARTTIME)
 {
     hour=IS_PARTTIME_HOUR;
@@ -14,5 +21,5 @@ else if(empCheck==IS_FULLTIME)
 {
     hour=IS_FULLTIME_HOUR;
 }
-let wage=hour*IS_WAGE_PERHOUR;
-console.log("Wage is"+wage);
+return hour;
+}
