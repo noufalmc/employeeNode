424eadfbe10d3de4d1abf40f4eecf010dfc6e401
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2022-02-11 06:25:03 +0530

    Added:UC&

--- employeePresneOrNot.js
+++ employeePresneOrNot.js
@@ -5,28 +5,62 @@ const IS_WAGE_PERHOUR=20;
 const MAX_HRS_IN_MONTH=160;
 const NO_OF_WORKINGDAYS=20;
 let totalEmpHrs=0;
-let tota_workingDays=0;
-let random;
+let tota_workingDays=wage=0;
+let counter=0;
+let random,empCheck;
 let employee=[];
 while(MAX_HRS_IN_MONTH<=160 && tota_workingDays<NO_OF_WORKINGDAYS)
 {
-  random=Math.floor(Math.random()*3);
-  if(random==1)
-  {
-      tota_workingDays+=1;
-      totalEmpHrs+=4;
-  }
-  else if(random==2)
-  {
-      tota_workingDays+=1;
-      totalEmpHrs+=8;
-  }
+  empCheck=Math.floor(Math.random()*3);
+  random=getWorkingHours(empCheck);
+  tota_workingDays++;
+  employee[counter]=calculateWage(random);
+  counter++;
+  totalEmpHrs+=random;
 }
-employee[0]=calculateWage(totalEmpHrs);
 function calculateWage(empHrs)
 {
         return empHrs*IS_WAGE_PERHOUR;
 }
+function sum(empWage)
+{
+        wage+=empWage;
+}
+function getWorkingHours(empCheck)
+{
+    switch(empCheck)
+    {
+        case 1:
+            return 4;
+        case 2:
+            return 8;
+        default:
+            return 0;    
+    }
+}
+function totalWage(totalWage,dailyWage)
+{
+    return totalWage+dailyWage;
+}
+employee.forEach(sum);
+counter=0;
+function mapWithDailyWage(dailyWage)
+{
+ counter++;
+ return counter +"=>"+dailyWage;
+}
+function fulltimeWage(empWage)
+{
+    return empWage.includes("160");
+}
+let empFullDay=employee.reduce(totalWage,0);
+let employeeFull=employee.map(mapWithDailyWage);
+employee.filter(n=>n==160).forEach(n=>console.log("Fully Earned => "+n));
+console.log("First=>"+employeeFull.find(fulltimeWage));
+console.log("Every=>"+employeeFull.every(fulltimeWage));
+console.log("Log=>"+employeeFull);
+console.log("Emp Full Day=>"+empFullDay);
+console.log("The wage is "+wage);
 console.log("Total Hour Is =>"+totalEmpHrs);
 console.log("Total Day Is =>"+tota_workingDays);
 console.log("The Employee array is "+employee);
